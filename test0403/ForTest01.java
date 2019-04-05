fd846f09203dcd5dc80413e49f08a4aaf3c940f4
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-05 16:34:03 +0900

    Create ForTest01.java

---
+++ test0403/ForTest01.java
@@ -0,0 +1,24 @@
+package test0403;
+
+public class ForTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i;
+			System.out.println("i="+ i  + ", sum="+sum);
+		}
+			System.out.println("sum="+ sum);
+			
+//		int sum1=0;
+//		
+//		for(int i=100; i>=1; i--) {
+//			sum1+=i;
+//			System.out.println("i="+ i  + ", sum="+sum1);
+//		}
+//		System.out.println("sum="+ sum1);
+	}
+
+}
