a914bc7b04d6e178f8dec7d0c047ab90a077caf2
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-05 16:34:07 +0900

    Create whileTest01.java

---
+++ test0403/whileTest01.java
@@ -0,0 +1,17 @@
+package test0403;
+
+public class whileTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		int i=1;
+		while(i<=100){
+			sum+=i;
+			i++;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
