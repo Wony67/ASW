a81f71fbbbbace789fdb6ba6f33f9a48ec4bb0c3
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-05 16:32:56 +0900

    Create paperTest.java

---
+++ test0401/paperTest.java
@@ -0,0 +1,17 @@
+package test0401;
+
+public class paperTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double speed;
+		double time;
+		double distance;
+		
+		speed=90.0;
+		time = 60.0;
+		distance = speed * time;
+		System.out.println(distance);
+	}
+
+}
