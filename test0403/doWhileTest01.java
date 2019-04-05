a003770b6e2ed903eff526cc7a7bef159db9160c
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-05 16:33:58 +0900

    Create doWhileTest01.java

---
+++ test0403/doWhileTest01.java
@@ -0,0 +1,20 @@
+package test0403;
+
+public class doWhileTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<10);
+		
+		i=1;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
