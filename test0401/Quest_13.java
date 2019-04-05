3828c40f825cd385925403458074addff65d8aa4
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-05 16:33:12 +0900

    Create Quest_13.java

---
+++ test0401/Quest_13.java
@@ -0,0 +1,20 @@
+package test0401;
+
+import java.util.Scanner;
+
+public class Quest_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.print("받은 돈 : ");
+		System.out.print("상품의 총액 : ");
+		
+		int money = s.nextInt();
+		int price = s.nextInt();
+		
+
+		//System.out.println("부가세 : "+ vat);
+	}
+
+}
