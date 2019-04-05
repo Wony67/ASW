75299d46403c4ae01948f30b7e31b3d7bf28c46c
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2019-04-05 16:34:13 +0900

    Create breakTest01.java

---
+++ test0404/breakTest01.java
@@ -0,0 +1,18 @@
+package test0404;
+
+public class breakTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int sum=0;
+//		int i;
+		for(int i=1; i<100;i++) {
+			if(i%2==0)
+				continue;
+			System.out.println(i);
+		}
+//		System.out.println();
+//		System.out.println("i="+i+", sum="+sum);
+	}
+
+}
